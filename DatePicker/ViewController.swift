796296142fa39569591cdb904a6d5f72326c1770
Mahmoud Waked <mwaked@gmail.com>
2020-09-15 20:55:12 +0200

    first commit

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var lablePreview: UILabel!
+    
+    var selectedDate = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lablePreview.numberOfLines = 3
+
+        selectedDate = datePicker.date.description(with: Locale(identifier: "ar"))
+        lablePreview.text = selectedDate
+        
+    }
+    
+    @IBAction func onPreviewDataclick(_ sender: Any) {
+        showAlert()
+    }
+    
+    @IBAction func onDateChangeListener(_ sender: Any) {
+        
+        selectedDate = datePicker.date.description(with: Locale(identifier: "ar"))        
+        lablePreview.text = selectedDate
+        
+    }
+    
+    
+    private func showAlert(){
+        let alert = UIAlertController(title: "Date Preview", message: selectedDate, preferredStyle: UIAlertController.Style.alert)
+
+        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
     }
 
-
 }
+    
+
 
